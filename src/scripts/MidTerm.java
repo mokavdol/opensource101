4738cabae0584201b035a290514744e5e385f464
moksh1 <mokavdol@gmail.com>
2022-04-21 18:49:21 +0900

    MidTerm 1차

--- src/scripts/MidTerm.java
+++ src/scripts/MidTerm.java
@@ -1,5 +1,30 @@
 package scripts;
 
-public class MidTerm {
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class MidTerm {
+	private String input_file;
+	private String query;
+	private MidTerm(String path, String query) {
+		this.query = query;
+		this.input_file = path;
+	}
+	
+	public void showSnippet() {
+		
+	}
 }
